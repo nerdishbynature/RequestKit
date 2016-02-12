2f6c3701170fa924059995e396e9d9542db3b531
Yohei Sugigami <y.sugigami@gmail.com>
2016-02-12 16:40:50 +0900

    Add target for OS X framework

---
+++ RequestKitOSX/RequestKitOSX.h
@@ -0,0 +1,19 @@
+//
+//  RequestKitOSX.h
+//  RequestKitOSX
+//
+//  Created by Piet Brauer on 26/10/15.
+//  Copyright © 2015 nerdish by nature. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RequestKitOSX.
+FOUNDATION_EXPORT double RequestKitOSXVersionNumber;
+
+//! Project version string for RequestKitOSX.
+FOUNDATION_EXPORT const unsigned char RequestKitOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RequestKitOSX/PublicHeader.h>
+
+
