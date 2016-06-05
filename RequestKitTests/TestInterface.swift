faacd63520363aad75293bc4893cc8af3e428704
Piet Brauer <piet@nerdishbynature.com>
2016-06-05 09:39:18 +0800

    Add test interface and JSON tests

---
+++ RequestKitTests/TestInterface.swift
@@ -0,0 +1,58 @@
+import RequestKit
+
+class TestInterface {
+    var configuration: Configuration {
+        return TestConfiguration("1234", url: "https://example.com")
+    }
+
+    func postJSON(session: RequestKitURLSession = NSURLSession.sharedSession(), completion: (response: Response<[String: AnyObject]>) -> Void) {
+        let router = JSONTestRouter.TestRoute(configuration)
+        router.postJSON(session, expectedResultType: [String: AnyObject].self) { json, error in
+            if let error = error {
+                completion(response: Response.Failure(error))
+            } else {
+                if let json = json {
+                    completion(response: Response.Success(json))
+                }
+            }
+        }
+    }
+}
+
+enum JSONTestRouter: JSONPostRouter {
+    case TestRoute(Configuration)
+
+    var configuration: Configuration {
+        switch self {
+        case .TestRoute(let config): return config
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .TestRoute:
+            return .POST
+        }
+    }
+
+    var encoding: HTTPEncoding {
+        switch self {
+        case .TestRoute:
+            return .JSON
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .TestRoute:
+            return "some_route"
+        }
+    }
+
+    var params: [String: String] {
+        switch self {
+        case .TestRoute:
+            return [:]
+        }
+    }
+}
