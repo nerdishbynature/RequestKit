439b2e9d85cfe40a456751d896655a3597bc56fa
Piet Brauer <piet@nerdishbynature.com>
2016-06-05 08:36:40 +0800

    Add error json to userInfo

--- RequestKit/JSONPostRouter.swift
+++ RequestKit/JSONPostRouter.swift
@@ -4,6 +4,8 @@ public protocol JSONPostRouter: Router {
     func postJSON<T>(session: RequestKitURLSession, expectedResultType: T.Type, completion: (json: T?, error: ErrorType?) -> Void) -> URLSessionDataTaskProtocol?
 }
 
+public let RequestKitErrorJSONKey = "RequestKitErrorJSONKey"
+
 public extension JSONPostRouter {
     public func postJSON<T>(session: RequestKitURLSession = NSURLSession.sharedSession(), expectedResultType: T.Type, completion: (json: T?, error: ErrorType?) -> Void) -> URLSessionDataTaskProtocol? {
         guard let request = request() else {
@@ -21,7 +23,11 @@ public extension JSONPostRouter {
         let task = session.uploadTaskWithRequest(request, fromData: data) { data, response, error in
             if let response = response as? NSHTTPURLResponse {
                 if !response.wasSuccessful {
-                    let error = NSError(domain: errorDomain, code: response.statusCode, userInfo: nil)
+                    var userInfo = [String: AnyObject]()
+                    if let data = data, json = try? NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as? [String: AnyObject] {
+                        userInfo[RequestKitErrorJSONKey] = json
+                    }
+                    let error = NSError(domain: errorDomain, code: response.statusCode, userInfo: userInfo)
                     completion(json: nil, error: error)
                     return
                 }
