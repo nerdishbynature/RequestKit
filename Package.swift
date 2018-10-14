9cff70ad708f9b4a312396d9dec6e60bff93b174
Haris Khaliq <haris.khaliq@thescore.com>
2018-10-13 22:17:57 -0400

    Update to Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
@@ -9,5 +9,6 @@ let package = Package(
     targets: [
         .target(name: "RequestKit", dependencies: []),
         .testTarget(name: "RequestKitTests", dependencies: ["RequestKit"])
-   ]
+   ],
+   swiftLanguageVersions: [.version("3.0"), .version("4.0"), .version("4.1"), .version("4.2")]
 )
