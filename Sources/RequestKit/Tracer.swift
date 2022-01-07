bb7be1a7728f87f59920035d26f2836ee68e9df1
Dmytro Milinevskyi <dmytro.milinevskyi@datadoghq.com>
2022-01-07 23:49:00 +0100

    import FoundationNetworking if it is possible

--- Sources/RequestKit/Tracer.swift
+++ Sources/RequestKit/Tracer.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public enum TracerOption: Int {
     case RequestHeaders, RequestBody, ResponseHeaders, ResponseBody
